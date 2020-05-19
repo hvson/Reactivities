e47798dd3b627c8d03a1fd4f5f5c98970dd1cbb4
Ha Van Son <havanson@gmail.com>
2020-05-19 14:45:27 +0700

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -53,6 +53,7 @@ namespace API
             app.UseRouting();
 
             app.UseAuthorization();
+            app.UseCors("CorsPolicy");
 
             app.UseEndpoints(endpoints =>
             {
